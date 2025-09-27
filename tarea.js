6be858c89c86fe46bdb04f10c89f5dd7ad64cbd2
paurub70 <rubioodltpaula@gmail.com>
2025-09-27 17:05:19 -0400

    Add files via upload

---
+++ tarea.js
@@ -0,0 +1,9 @@
+const nombre = "Ana paula";
+const añodenacimiento = 2007;
+const añoactual = 2025;
+
+const edad = añoactual - añodenacimiento;
+
+console.log(`hola, mi nombre es ${nombre} y tengo ${edad} años.`);
+
+
